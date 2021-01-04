4d716fa14e778c323443aef8f3b91052c443c7de
vebdeveloper97 <vebdeveloper571632@mail.ru>
2021-01-04 10:58:38 +0500

    admin table migrations

---
+++ migrations/m210104_053755_create_admin_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%admin}}`.
+ */
+class m210104_053755_create_admin_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%admin}}', [
+            'id' => $this->primaryKey(),
+            'test' => $this->char(50),
+            'text' => $this->text(),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%admin}}');
+    }
+}
