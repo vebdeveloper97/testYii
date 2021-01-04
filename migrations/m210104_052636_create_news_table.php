a49d6fb847ab683d94052762e9476a52ebff9afd
vebdeveloper97 <vebdeveloper571632@mail.ru>
2021-01-04 10:29:15 +0500

    news migrations created

---
+++ migrations/m210104_052636_create_news_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%news}}`.
+ */
+class m210104_052636_create_news_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%news}}', [
+            'id' => $this->primaryKey(),
+            'title' => $this->char(50)->notNull(),
+            'content' => $this->text(),
+            'files' => $this->char(100),
+            'status' => $this->integer(),
+            'created_at' => $this->integer(),
+            'updated_at' => $this->integer(),
+            'created_by' => $this->integer(),
+            'updated_by' => $this->integer(),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%news}}');
+    }
+}
