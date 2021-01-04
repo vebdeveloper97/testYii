e1c435dac2c304c03897d7e2b4b5e78690fbfcf6
vebdeveloper97 <vebdeveloper571632@mail.ru>
2021-01-04 10:59:45 +0500

    Views files

---
+++ modules/admin/views/admin/_search.php
@@ -0,0 +1,34 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\AdminSearch */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="admin-search">
+
+    <?php $form = ActiveForm::begin([
+        'action' => ['index'],
+        'method' => 'get',
+        'options' => [
+            'data-pjax' => 1
+        ],
+    ]); ?>
+
+    <?= $form->field($model, 'id') ?>
+
+    <?= $form->field($model, 'test') ?>
+
+    <?= $form->field($model, 'text') ?>
+
+    <div class="form-group">
+        <?= Html::submitButton(Yii::t('app', 'Search'), ['class' => 'btn btn-primary']) ?>
+        <?= Html::resetButton(Yii::t('app', 'Reset'), ['class' => 'btn btn-outline-secondary']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
