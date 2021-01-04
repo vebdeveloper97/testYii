9f9dbe421cefe9df75601c2c4aaa70c492db2ec8
vebdeveloper97 <vebdeveloper571632@mail.ru>
2021-01-04 10:58:25 +0500

    config.php settings

--- config/web.php
+++ config/web.php
@@ -11,6 +11,14 @@ $config = [
         '@bower' => '@vendor/bower-asset',
         '@npm'   => '@vendor/npm-asset',
     ],
+    'modules' => [
+        'test' => [
+            'class' => 'app\modules\test\Module',
+        ],
+        'admin' => [
+            'class' => 'app\modules\admin\Module',
+        ],
+    ],
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
