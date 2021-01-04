9d08a6c8a4ec7f2b7d3f61e7286d9a295a5a30ac
vebdeveloper97 <vebdeveloper571632@mail.ru>
2021-01-04 07:46:01 +0500

    FIrst Commit

---
+++ vuejs/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <notes />
+  </div>
+</template>
+
+<script>
+import Notes from './components/Notes'
+
+export default {
+  name: 'App',
+  components: {
+    notes: Notes
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vuejs/src/components/Notes.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="tc-notes-wrapper">
+    <add-button
+      @addNote="addNote"
+    />
+    <div class="tc-notes">
+      <note
+          v-for="(note, index) in notes"
+          :key="index"
+          :note="note"
+          @deleteNote="deleteNote"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import AddButton from "@/components/AddButton";
+import Note from "@/components/Note";
+
+export default {
+  name: "Notes",
+  components: {
+    addButton: AddButton,
+    note: Note
+  },
+  data(){
+    return {
+      notes: [
+        {
+          title: 'sunt aut facere repellat',
+          body: 'uia et suscipit suscipit recusandae consequuntur expedita et cum reprehenderit molestiae ut ut quas totam nostrum rerum est autem sunt rem eveniet architecto'
+        },
+        {
+          title: 'qui est esse',
+          body: 'est rerum tempore vitae<br>nsequi sint nihil reprehenderit dolor beatae ea dolores neque <br>fugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis<br>qui aperiam non debitis possimus qui neque nisi nulla'
+        },
+        {
+          title: 'nesciunt quas odio',
+          body: 'repudiandae veniam quaerat sunt sed alias aut fugiat sit autem sed est'
+        },
+        {
+          title: 'This is a demo note',
+          body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi corrupti officiis alias tenetur, tenetur iste maxime laudantium?'
+        },
+        {
+          title: 'qui est esse',
+          body: 'est rerum tempore vitae<br>nsequi sint nihil reprehenderit dolor beatae ea dolores neque <br>fugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis<br>qui aperiam non debitis possimus qui neque nisi nulla'
+        },
+      ]
+    }
+  },
+  methods: {
+    addNote(){
+      this.notes.unshift({title: '', body: ''})
+    },
+    deleteNote(note){
+      this.notes.splice(this.notes.indexOf(note),1)
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.tc-notes-wrapper {
+  .tc-notes {
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+    margin: 0 auto;
+  }
+}
+</style>
